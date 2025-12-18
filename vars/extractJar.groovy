98a7722fd079e7e95fbc9ba13b12191108364066
altyapistaj <altyapi_staj@proton.me>
2025-12-18 16:13:16 +0300

    Update gitCheckout.groovy

--- vars/extractJar.groovy
+++ vars/extractJar.groovy
@@ -2,7 +2,7 @@ def call (Map buildParams){
     //TODO: linux/bash komutlarına çevirilecek.
     powershell '''           
            $jar = Get-ChildItem -Path "target" -Filter "*.jar" | Select-Object -First 1
-           java "-Djarmode=layertools" -jar $jar.FullName extract --destination "${layersDir}"
+           java "-Djarmode=layertools" -jar $jar.FullName extract"
            '''
 }
 
