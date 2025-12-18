40401a0960f34302a4f6f2381e485721d8a04e24
altyapistaj <altyapi_staj@proton.me>
2025-12-18 13:17:34 +0300

    İnitial commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,9 @@
-def call() {
+def call(Map stageParams) {
     def scmVars = checkout([
             $class: 'GitSCM',
             branches: [[name: env.BRANCH_NAME ]],
-            userRemoteConfigs: [[url: "https://github.com/altyapistaj/demo-service.git"]]
+            userRemoteConfigs: [[url: "https://github.com/"  + stageParams.gitPathName + "/" + stageParams.jobName + ".git"]],
+            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.customWorkspace]]
     ])
     env.GIT_COMMIT = scmVars.GIT_COMMIT
     env.GIT_BRANCH = scmVars.GIT_BRANCH
