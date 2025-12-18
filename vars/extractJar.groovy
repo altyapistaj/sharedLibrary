7fbb3842fc180058299846643fd3e958aeb58c71
altyapistaj <altyapi_staj@proton.me>
2025-12-18 16:05:05 +0300

    Update gitCheckout.groovy

--- vars/extractJar.groovy
+++ vars/extractJar.groovy
@@ -1,14 +1,8 @@
 def call (Map buildParams){
     //TODO: linux/bash komutlarına çevirilecek.
-    powershell '''
-           if (Test-Path '${layersDir}) { Remove-Item -Recurse -Force '${layersDir}' }
-           New-Item -ItemType Directory -Force -Path '${layersDir}' | out-Null
-           
-           \$jar = Get-ChildItem -Path 'target' -Filter '*.jar' | Select-Object -First 1
-           if(-not \$jar) {throw 'target klasorunde .jar yok.}
-           
-           Write-Host "Jar bulundu \$([\$jar].FullName)"
-           java Djarmode0layertools -jar \$jar.FullName extract --destination '${layersDir}'
+    powershell '''           
+           $jar = Get-ChildItem -Path "target" -Filter "*.jar" | Select-Object -First 1
+           java Djarmode=layertools -jar $jar.FullName extract --destination "${layersDir}"
            '''
 }
 
