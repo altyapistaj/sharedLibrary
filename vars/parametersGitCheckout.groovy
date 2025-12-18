1282602e0c8519162725f00f23171adb97db7d65
altyapistaj <altyapi_staj@proton.me>
2025-12-18 14:05:43 +0300

    Update gitCheckout.groovy

--- vars/parametersGitCheckout.groovy
+++ vars/parametersGitCheckout.groovy
@@ -2,12 +2,7 @@ def call()
 {
     properties([
             parameters([
-                    booleanParam(name: 'Checkout', defaultValue:true, description: ''),
-                    choice(name: "JdkVersion",
-                            description:"Java Version Selection",
-                            type: 'PT_SINGLE_SELECT',
-                            value: 'openjdk11,jdk-17,JDK8',
-                            defaultValue:'openjdk11' )
+                    booleanParam(name: 'Checkout', defaultValue:true, description: 'Check for Checkout')
             ])
     ])
 
