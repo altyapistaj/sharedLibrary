acb1a93a8a14fafa7d67fa534f02e08b6e08ac7b
altyapistaj <altyapi_staj@proton.me>
2025-12-23 14:31:05 +0300

    Updated pipelineTest.groovy

--- vars/parametersDeployToZot.groovy
+++ vars/parametersDeployToZot.groovy
@@ -2,26 +2,16 @@ def call()
 {
     properties([
             parameters([
-                    booleanParam(name: 'CleanWorkspace', defaultValue:false, description: ''),
-                    booleanParam(name: 'Checkout', defaultValue:true, description: ''),
-                    booleanParam(name: 'Build', defaultValue:true, description: '' ),
-                    booleanParam(name: 'DeployToNexus', defaultValue:true, description: '' ),
-                    extendedChoice(name: "JdkVersion",
-                            description:"Java Version Selection",
-                            type: 'PT_SINGLE_SELECT',
-                            value: 'openjdk11,jdk-17,JDK8',
-                            defaultValue:'openjdk11' )
+                    booleanParam(name: 'Checkout', defaultValue:true, description: 'Check for Checkout'),
+                    booleanParam(name: 'Build', defaultValue:true, description: 'Check for Build'),
+                    booleanParam(name: 'Extract', defaultValue:true, description: 'Check for Extract'),
+                    booleanParam(name: 'dockerBuild', defaultValue:true, description: 'Check for Docker build'),
+                    booleanParam(name: 'pushToZot', defaultValue:true, description: 'Check for push to Zot')
             ])
     ])
 
 
 
 
-    stage('Echo Parameters') {
-        echo "CleanWorkspace=" + params.Clean_Workspace
-        echo "Checkout=" + params.Checkout
-        echo "Build=" + params.Build
-        echo "DeployToNexus=" + params.DeployToNexus
-        echo "Java Version=" + params.JdkVersion
-    }
+
 }
