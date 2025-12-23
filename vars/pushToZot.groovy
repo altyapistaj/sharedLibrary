cdbd8e660300313419faf50b27ca8bbf2e6aee4c
altyapistaj <altyapi_staj@proton.me>
2025-12-23 09:43:59 +0300

    Added pushToZot.groovy Updated parametersGitCheckout.groovy

--- vars/pushToZot.groovy
+++ vars/pushToZot.groovy
@@ -1,4 +1,4 @@
 def call (){
     //TODO: linux/bash komutlarına çevirilecek.
-    bat "docker push localhost:5000/demo-service:latest"
+    bat "docker push localhost:5000/${env.JOB_NAME}:latest"
 }
