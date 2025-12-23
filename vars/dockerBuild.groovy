8d170203c810b878a51fc2161be0d5abf2a69ca6
altyapistaj <altyapi_staj@proton.me>
2025-12-23 09:47:24 +0300

    Updated dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call (){
     //TODO: linux/bash komutlarına çevirilecek.
-    bat "docker build -f Dockerfile . --tag localhost:5000/demo-service:latest"
+    bat "docker build -f Dockerfile . --tag localhost:5000/${env.JOB_NAME}:latest"
 }
 
