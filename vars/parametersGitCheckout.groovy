ce8afaeb1536392fa9450ec348f81d6aa1b8a6c4
altyapistaj <altyapi_staj@proton.me>
2025-12-23 08:23:45 +0300

    Added pushToZot.groovy Updated parametersGitCheckout.groovy

--- vars/parametersGitCheckout.groovy
+++ vars/parametersGitCheckout.groovy
@@ -5,7 +5,8 @@ def call()
                     booleanParam(name: 'Checkout', defaultValue:true, description: 'Check for Checkout'),
                     booleanParam(name: 'Build', defaultValue:true, description: 'Check for Build'),
                     booleanParam(name: 'Extract', defaultValue:true, description: 'Check for Extract'),
-                    booleanParam(name: 'dockerBuild', defaultValue:true, description: 'Check for Docker Build')
+                    booleanParam(name: 'dockerBuild', defaultValue:true, description: 'Check for Docker build'),
+                    booleanParam(name: 'pushToZot', defaultValue:true, description: 'Check for push to Zot')
             ])
     ])
 
---
+++ vars/pushToZot.groovy
@@ -0,0 +1,4 @@
+def call (){
+    //TODO: linux/bash komutlarına çevirilecek.
+    bat "docker push localhost:5000/demo-service:latest"
+}
