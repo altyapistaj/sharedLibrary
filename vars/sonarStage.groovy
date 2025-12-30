6cae4747d3f2c09988ca04d5cf993c378cd3593d
altyapistaj <altyapi_staj@proton.me>
2025-12-30 13:22:15 +0300

    Updated sonarStage.groovy

--- vars/sonarStage.groovy
+++ vars/sonarStage.groovy
@@ -1,37 +1,49 @@
-def call (Map sonarParams){
+def call (Map sonarParams = [:]){
 
     def varProject = sonarParams.projectName
     def varProjectRootName = sonarParams.customWorkspace
     def varProjectCombine = sonarParams.jobName
 
+    def propsFileName="${varProjectCombine}.sonar-project.properties"
+
     def varProjectPath = "/workspace/$varProject"
     def varProjectRootPath = "$varProjectRootName"
     def varProjectRootSQWorkspacePath = "$varProjectRootPath"
 
-    echo "varProjectRootName=$varProjectRootName"
-    echo "varProjectRootPath=$varProjectRootPath"
-    echo "varProjectRootSQWorkspacePath=$varProjectRootSQWorkspacePath"
+    echo "varProject=${varProjectRootName}"
+    echo "varProjectRootName=${varProjectRootName}"
+    echo "varProjectCombine=${varProjectCombine}"
 
-    //TODO: sh komutlarına çevirilecek
-    bat '''
-        sonarprojectfile='''+varProjectPath+'''/'''+sonarParams.jobName+'''.sonar-project.properties
-        echo "sonar.projectKey='''+varProjectCombine+'''" > $sonarprojectfile
-        echo "sonar.projectName='''+varProjectCombine+'''" >> $sonarprojectfile
-        echo "sonar.projectVersion=$BUILD_NUMBER" >> $sonarprojectfile
-        echo "sonar.sourceEncoding=UTF-8" >> $sonarprojectfile 
-        echo "sonar.sources='''+varProjectRootPath+'''" >> $sonarprojectfile
-        echo "sonar.projectBaseDir='''+varProjectRootSQWorkspacePath+'''" >> $sonarprojectfile
-        echo "sonar.java.binaries='''+varProjectRootPath+'''" >> $sonarprojectfile
-        echo "sonar.java.coveragePlugin=jacoco" >> $sonarprojectfile
-            
-             /var/lib/jenkins/workspace/unit.sh $sonarprojectfile $varProjectPath
-        
-        '''
-    //TODO: sh komutlarına çevirilecek
-    ws("$varProjectRootSQWorkspacePath"){
-        def scannerHome = tool name: 'sonar-scanner-5.4.0.6343', type:'hudson.plugins.sonar.SonarRunnerInstallation';
-        withSonarQubeEnv('SonarQube-25.12'){
-            bat " ${scannerHome}/bin/sonar-scanner -Dproject.settings=${varProjectPath}/"+sonarParams.jobName+".sonar-project.properties"
-        }
-    }
+   ws("${varProjectRootName}") {
+       def props = """\
+sonar.projectKey=${varProjectCombine}
+sonar.projectName=${varProjectCombine}
+sonar.projectVersion=${env.BUILD_NUMBER ?: "1"}
+
+
+sonar.sourceEncoding=UTF-8
+
+sonar.sources=src/main/java
+sonar.tests=src/test/java
+
+sonar.java.binaries=target/classes
+
+sonar.junit.reportPaths=target/surefire-reports
+sonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml
+""".stripIndent()
+       writeFile file: propsFileName, text: props
+
+       echo "Created sonar properties at: ${pwd()}/${propsFileName}"
+
+       def scannerHome = tool(
+               name: (sonarParams.scannerToolName ?: 'sonar-scanner-25.12.0.117093'),
+               type: 'hudson.plugins.sonar.sonarRunnerInstallation'
+       )
+
+       withSonerQubeEnv('sq1')
+
+       bat """
+        "${scannerHome}\\bin\\sonar-scanner.bat -Dproject.settings="${propsFileName}""
+""".trim()
+   }
 }
