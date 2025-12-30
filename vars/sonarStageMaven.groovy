bd983af921b57642c91dd9249c51b553ac187a4a
altyapistaj <altyapi_staj@proton.me>
2025-12-30 15:23:15 +0300

    Updated sonarStageMaven.groovy

--- vars/sonarStageMaven.groovy
+++ vars/sonarStageMaven.groovy
@@ -1,6 +1,6 @@
 def call (Map sonarParams = [:]){
 
-    def sonarServerName = sonarParams.sonarServerName  ?: 'sq1'
+    def sonarServerName = sonarParams.sonarServerName  ?: 'sq1' //jenkins system ayarlarında tanımlanan SonarQube adı
     def pomPath = sonarParams.pom ?: 'pom.xml'
 
     withSonarQubeEnv(sonarServerName){
