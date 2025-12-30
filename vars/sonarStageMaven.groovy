42e9b6649d965aaf7843ea9553360f962610e990
altyapistaj <altyapi_staj@proton.me>
2025-12-30 14:21:28 +0300

    Updated sonarStageMaven.groovy

--- vars/sonarStageMaven.groovy
+++ vars/sonarStageMaven.groovy
@@ -3,6 +3,9 @@ def call (Map sonarParams = [:]){
     def sonarServerName = sonarParams.sonarServerName  ?: 'sq1'
     def pomPath = sonarParams.pom ?: 'pom.xml'
 
+    bat 'cd'
+    bat 'dir'
+    bat 'type pom.xml | findstr /i "sonar-maven-plugin"'
 
     withSonarQubeEnv(sonarServerName){
         bat """
