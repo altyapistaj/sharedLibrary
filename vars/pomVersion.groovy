ad2465fceeb3841ada89ea508d306f48484c5959
altyapistaj <altyapi_staj@proton.me>
2026-01-22 14:16:28 +0300

    pomVersion.groovy updated

--- vars/pomVersion.groovy
+++ vars/pomVersion.groovy
@@ -2,7 +2,7 @@ def call() {
     def version = bat(
             script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout",
             returnStdout : true
-    ).trim
+    ).trim()
 
     env.APP_VERSION = version
     echo "Maven version =${env.APP_VERSION}"
