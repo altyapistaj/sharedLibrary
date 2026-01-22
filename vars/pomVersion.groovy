3e03947f0e98dc156a97b344d6b9e7e2935b02ab
altyapistaj <altyapi_staj@proton.me>
2026-01-22 15:41:31 +0300

    sonarStage.groovy updated

--- vars/pomVersion.groovy
+++ vars/pomVersion.groovy
@@ -2,6 +2,6 @@ def call() {
     def pom = readMavenPom file: 'pom.xml'
     env.APP_VERSION = pom.version
 
-    echo "Maven version =${env.APP_VERSION}"
+    echo "Maven version ${env.APP_VERSION}"
 
 }
