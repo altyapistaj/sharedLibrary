ca37e790588ac263cc86d9f6a60cc25319ed0510
altyapistaj <altyapi_staj@proton.me>
2026-01-22 13:38:17 +0300

    pipelineNPMDeployToZot.groovy updated

--- vars/packageJSON.groovy
+++ vars/packageJSON.groovy
@@ -1,7 +1,6 @@
 def call(Map jsonParams) {
-    def packageJson=""
-    packageJson = readJSON file: 'package.json'
-    version = packageJson.version
+    def packageJson = readJSON file: 'package.json'
+    def version = packageJson.version
     echo "version="+version
     return version
 
