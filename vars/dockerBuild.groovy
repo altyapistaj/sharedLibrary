94a6ca2a2c22997679bbbc94178ff183677873d0
altyapistaj <altyapi_staj@proton.me>
2026-01-28 14:25:21 +0300

    TODOs are removed

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,4 @@
 def call (){
-    //TODO: linux/bash komutlarına çevirilecek.
     sh "docker build -f Dockerfile . --tag localhost:5000/${env.JOB_NAME.tokenize('/')[1]}:${env.APP_VERSION}"
 }
 
--- vars/pushToZot.groovy
+++ vars/pushToZot.groovy
@@ -1,4 +1,3 @@
 def call (){
-    //TODO: linux/bash komutlarına çevirilecek.
     sh "docker push localhost:5000/${env.JOB_NAME.tokenize('/')[1]}:${env.APP_VERSION}"
 }
