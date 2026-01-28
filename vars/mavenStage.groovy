dce91a8ee367524006d4e6f9ee5fd504fb0f85d9
altyapistaj <altyapi_staj@proton.me>
2026-01-28 11:28:59 +0300

    script in mavenStage.groovy has been converted to shell script

--- vars/mavenStage.groovy
+++ vars/mavenStage.groovy
@@ -1,4 +1,4 @@
 def call (Map buildParams){
     //TODO: linux/bash komutlarına çevirilecek.
-    bat "mvn ${buildParams.text} -f ${buildParams.pom}"
+    sh "mvn ${buildParams.text} -f ${buildParams.pom}"
 }
